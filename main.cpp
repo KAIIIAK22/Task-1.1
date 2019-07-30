8a1d569b401396b2f0196fb54b809325dcdefe87
Pavel Poltaves <dzhura17@yandex.ru>
2019-07-30 16:10:06 +0300

    Simple CPP Calculator
    Sum two values :3

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Vvedite chislo 1: ";
+    cin >> a;
+    cout << "Vvedite chislo 2: ";
+    cin >> b;
+    cout << "Resultat: " << a+b <<endl;
+    return 0;
+}
